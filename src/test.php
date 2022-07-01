dbea39b41f02354c6ded0ea35b0c427eaa4ab533
Brian <briansalo1997@gmail.com>
2022-07-01 21:31:04 +0800

    for addition

--- src/test.php
+++ src/test.php
@@ -5,8 +5,9 @@ namespace test;
 class test
 {
 
-    public function calculateData(int $first = 1, int $second = 2)
+    public function plus(int $first = 1, int $second = 2)
     {
         return $first + $second;
     }
+
 }
